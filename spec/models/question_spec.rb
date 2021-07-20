1050bf579c4393519f8cdcbfd84ccaf3142d5051
Мария <golovneva.maria@yandex.ru>
2021-07-20 18:18:08 +0300

    rspec fixes

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
-require 'rails_helper'
-
-RSpec.describe Question, type: :model do
+describe Question, type: :model do
   it { should have_many(:answers) }
 
   it { should validate_presence_of :title }
