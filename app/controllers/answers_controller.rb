dd1f13c3b75b6234c09f2f877e5fed6f22fb9c61
Мария <golovneva.maria@yandex.ru>
2021-07-26 13:27:49 +0300

    action create created for Answer controller

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -1,12 +1,33 @@
 # frozen_string_literal: true
 
 class AnswersController < ApplicationController
+  before_action :find_question, only: %i[new create]
+
   def show
     @answer = Answer.find(params[:id])
   end
 
   def new
-    @question = Question.find(params[:question_id])
     @answer = Answer.new
+  end
+
+  def create
+    @answer = @question.answers.build(answer_params)
+
+    if @answer.save
+      redirect_to @answer
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def find_question
+    @question = Question.find(params[:question_id])
+  end
+
+  def answer_params
+    params.require(:answer).permit(:body)
   end
 end
