8d459f68461821cfa50735e8b16a7831b364d5c7
Мария <golovneva.maria@yandex.ru>
2021-07-26 11:43:19 +0300

    show action for Question controller implemented

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class QuestionsController < ApplicationController
+  def show
+    @question = Question.find(params[:id])
+  end
+
   def new
     @question = Question.new
   end
