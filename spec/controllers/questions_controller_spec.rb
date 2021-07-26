2740bcde09f2337a11715181109093f5c423491a
Мария <golovneva.maria@yandex.ru>
2021-07-26 11:36:30 +0300

    test for show action for Question controller created

--- spec/controllers/questions_controller_spec.rb
+++ spec/controllers/questions_controller_spec.rb
@@ -40,4 +40,16 @@ describe QuestionsController, type: :controller do
       end
     end
   end
+
+  describe 'GET #show' do
+    before { get :show, params: { id: question } }
+
+    it 'assigns the requested question to @question' do
+      expect(assigns(:question)).to eq question
+    end
+
+    it 'renders show view' do
+      expect(response).to render_template :show
+    end
+  end
 end
