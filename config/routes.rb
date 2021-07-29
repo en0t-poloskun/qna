027ee2bbb81a7c155c0c7762d8291d3411c4f654
Мария <golovneva.maria@yandex.ru>
2021-07-29 15:03:32 +0300

    gem devise added to project

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  devise_for :users
+  root to: 'questions#index'
+
   resources :questions, only: %i[new create show index] do
     resources :answers, shallow: true, only: %i[new create show]
   end
