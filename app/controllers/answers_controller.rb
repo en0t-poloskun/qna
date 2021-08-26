e5a18b22e3339946b48ebe6bd7a422d80ce8d4d1
Мария <golovneva.maria@yandex.ru>
2021-08-26 11:31:48 +0300

    story implemented: authenticated user can add an answer with attached files

--- app/controllers/answers_controller.rb
+++ app/controllers/answers_controller.rb
@@ -28,10 +28,10 @@ class AnswersController < ApplicationController
   private
 
   def find_answer
-    @answer = Answer.find(params[:id])
+    @answer = Answer.with_attached_files.find(params[:id])
   end
 
   def answer_params
-    params.require(:answer).permit(:body)
+    params.require(:answer).permit(:body, files: [])
   end
 end
