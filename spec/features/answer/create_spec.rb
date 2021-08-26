94fee37b2c5980d8a25472cf2b230350a8363575
Мария <golovneva.maria@yandex.ru>
2021-08-26 11:08:43 +0300

    acceptance test created: authenticated user adds an answer with attached files

--- spec/features/answer/create_spec.rb
+++ spec/features/answer/create_spec.rb
@@ -31,6 +31,20 @@ feature 'User can create answer', "
 
       expect(page).to have_content "Body can't be blank"
     end
+
+    scenario 'adds an answer with attached files' do
+      fill_in 'Body', with: 'text text text'
+
+      attach_file 'Files', ["#{Rails.root}/spec/rails_helper.rb", "#{Rails.root}/spec/spec_helper.rb"]
+      click_on 'Add'
+
+      expect(page).to have_current_path question_path(question), ignore_query: true
+
+      within '.answers' do
+        expect(page).to have_link 'rails_helper.rb'
+        expect(page).to have_link 'spec_helper.rb'
+      end
+    end
   end
 
   scenario 'unauthenticated user tries to add an answer' do
