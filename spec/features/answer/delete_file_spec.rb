fdd13fba64c3e146bb9a7602e51219073114dee6
Мария <golovneva.maria@yandex.ru>
2021-08-26 18:54:06 +0300

    acceptance tests created: Author of answer can delete attached files

--- spec/features/answer/delete_file_spec.rb
+++ spec/features/answer/delete_file_spec.rb
@@ -1,22 +1,21 @@
 # frozen_string_literal: true
 
-feature 'Author of question can delete attached files', "
+feature 'Author of answer can delete attached files', "
   In order to remove unnecessary files
-  As the author of the question
+  As the author of the answer
   I'd like to be able to delete attached files
 " do
   given(:user) { create(:user) }
   given(:question) { create(:question) }
+  given(:answer) do
+    create(:answer, question: question, files: [Rack::Test::UploadedFile.new(Rails.root.join('Gemfile.lock'))])
+  end
 
   describe 'authenticated user' do
-    background do
-      sign_in(user)
+    background { sign_in(user) }
 
-      question.files.attach(Rack::Test::UploadedFile.new(Rails.root.join('Gemfile.lock')))
-    end
-
-    scenario 'deletes files attached to his question', js: true do
-      user.questions.push(question)
+    scenario 'deletes files attached to his answer', js: true do
+      user.answers.push(answer)
 
       visit question_path(question)
       page.accept_confirm { click_link 'Delete file' }
@@ -24,7 +23,7 @@ feature 'Author of question can delete attached files', "
       expect(page).not_to have_link 'Gemfile.lock'
     end
 
-    scenario "tries to delete files attached to someone else's question" do
+    scenario "tries to delete files attached to someone else's answer" do
       visit question_path(question)
 
       expect(page).not_to have_link 'Delete file'
