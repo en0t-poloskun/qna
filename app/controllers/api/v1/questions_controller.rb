dd5fd396d17dfa61459ee39a556b788bc4b001e9
Мария <golovneva.maria@yandex.ru>
2021-09-16 13:13:21 +0300

    api for one question implemented

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -3,11 +3,14 @@
 module Api
   module V1
     class QuestionsController < Api::V1::BaseController
-      authorize_resource
+      load_and_authorize_resource
 
       def index
-        @questions = Question.all
         render json: @questions
+      end
+
+      def show
+        render json: @question
       end
     end
   end
